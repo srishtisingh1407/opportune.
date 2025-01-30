d6a299f2bd8fa95679750650b5c83a45abe98da5
Srishtisingh1407 <srishtisingh140704@gmail.com>
2025-01-30 23:28:08 +0530

    header bg

--- components/header.jsx
+++ components/header.jsx
@@ -1,17 +1,37 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import React from "react";
+import { Button } from "./ui/button";
+import {
+  PenBox,
+  LayoutDashboard,
+  FileText,
+  GraduationCap,
+  ChevronDown,
+  StarsIcon,
+} from "lucide-react";
+import Link from "next/link";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Image from "next/image";
 
-function Header() {
+
+export default async function Header() {
+ 
+
   return (
-    <div>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
+    <header className="fixed top-0 w-full border-b bg-background/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60">
+      <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
+        <Link href="/">
+         <h1 className="font-bold text-2xl">Opportune<span className="text-cyan-800">.</span></h1>
+        </Link>
+
+      
+      
+      </nav>
+    </header>
   );
 }
-
-export default Header;
