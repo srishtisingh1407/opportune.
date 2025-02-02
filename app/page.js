8d60d2bb58c29cace959356635958307c9db1472
Srishtisingh1407 <srishtisingh140704@gmail.com>
2025-02-02 14:38:53 +0530

    hero section

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
+import HeroSection from "@/components/hero";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <div>
-    hello
-    <Button>sigma boy</Button>
-   </div>
+  <>
+   <div className="grid-background"></div>
+
+{/* Hero Section */}
+<HeroSection />
+  </>
   );
 }
