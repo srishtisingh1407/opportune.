57b09cc1832fa8feeaa1c9975c8221b9555ad3d6
Srishtisingh1407 <srishtisingh140704@gmail.com>
2025-02-06 19:53:49 +0530

    changes on landoing page

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Toaster } from "sonner";
 import Header from "@/components/header";
 import { ThemeProvider } from "@/components/theme-provider";
-import { dark } from "@clerk/themes";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider
-      appearance={{
-        baseTheme: dark,
-      }}
+     
     >
       <html lang="en" suppressHydrationWarning>
         <head>
@@ -33,11 +30,11 @@ export default function RootLayout({ children }) {
           >
             <Header />
             <main className="min-h-screen">{children}</main>
-            <Toaster richColors />
+           
 
             <footer className="bg-muted/50 py-12">
               <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with 💗 by RoadsideCoder</p>
+                <p>Made with 🎀 by Srishti</p>
               </div>
             </footer>
           </ThemeProvider>
