e58d24b39a3cdc808cc361b3127a74634f4a47b7
Srishtisingh1407 <srishtisingh140704@gmail.com>
2025-02-11 14:15:43 +0530

    onboarding scheme

--- app/(main)/onboarding/_components/onboardingform.jsx
+++ app/(main)/onboarding/_components/onboardingform.jsx
@@ -1,13 +1,49 @@
-"use client"
+"use client";
 
-import React from 'react'
-import { useForm } from 'react-hook-form'
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { oboardingSchema } from "@/app/lib/schema";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useRouter } from "next/router";
 
-const OnboardingForm = ({industries}) => {
-  useForm()
+const OnboardingForm = ({ industries }) => {
+  const [selectedIndustry, setSelectedIndustry] = useState(null);
+
+  const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    watch,
+  } = useForm({
+    resolver: zodResolver(oboardingSchema),
+  });
   return (
-    <div>OnboardingForm</div>
-  )
-}
+    <div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
 
-export default OnboardingForm
+export default OnboardingForm;
